57db7867e5acbb10febb1c217d283d15877a19df
dudagiglioli <dudaguilice@gmail.com>
2023-08-23 08:56:34 -0300

    header and styles :construction:

--- restaurante/src/pages/Cardapio/index.tsx
+++ restaurante/src/pages/Cardapio/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Cardapio.module.scss';
-import Logo from '../../assets/logo.svg';
+import Logo from 'assets/logo.svg';
 
 export default function Cardapio(){
     return(
@@ -7,6 +7,16 @@ export default function Cardapio(){
            <nav className={styles.menu}>
               <img src={Logo} alt="Logo do Restaurante" />
            </nav>
+           
+           <header className={styles.header}>
+<div className={styles.header__text}>
+    "A casa do código e da massa!!!"
+</div>
+           </header>
+
+           <section>
+               <h3>Cardápio</h3>
+           </section>
        </main>
     );
 }
